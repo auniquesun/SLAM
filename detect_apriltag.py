b5586869c462dfd8c69ae3e13c94bffdd07a76c3
Jerry Sun <Mr.sunhy@outlook.com>
2019-08-15 20:46:08 +0800

    fix bug

--- detect_apriltag.py
+++ detect_apriltag.py
@@ -4,20 +4,26 @@
 import apriltag
 import cv2
 
-#img = cv2.imread('tag-standard-41h12.png', cv2.IMREAD_GRAYSCALE)
-img = cv2.imread('tag-standard-41h12.png', cv2.IMREAD_GRAYSCALE)
-options = apriltag.DetectorOptions(families='tagstandard41h12',
-            border=1,
-            nthreads=4,
-            quad_decimate=1.0,
-            quad_blur=0.0,
-            refine_edges=True,
-            refine_decode=False,
-            refine_pose=False,
-            debug=False,
-            quad_contours=True)
-detector = apriltag.Detector()
+def start():
+    #img = cv2.imread('tag-standard-41h12.png', cv2.IMREAD_GRAYSCALE)
+    img_name = input('input the image name: ')
+    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+    options = apriltag.DetectorOptions(families='tagstandard41h12',
+                border=1,
+                nthreads=4,
+                quad_decimate=1.0,
+                quad_blur=0.0,
+                refine_edges=True,
+                refine_decode=False,
+                refine_pose=False,
+                debug=False,
+                quad_contours=True)
+    detector = apriltag.Detector()
 
-results = detector.detect(img)
+    results = detector.detect(img)
 
-print(results)
+    print(results)
+    print('number of detections: ', len(results))
+
+if '__main__' == __name__:
+    start()
